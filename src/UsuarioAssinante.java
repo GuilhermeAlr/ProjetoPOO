52a321474f89cbd5ac102447a0fd3b924534d720
Ginoventa <164910701+ginoventa@users.noreply.github.com>
2024-10-24 13:25:01 -0300

    Update UsuarioAssinante.java

--- src/UsuarioAssinante.java
+++ src/UsuarioAssinante.java
@@ -1,7 +1,14 @@
+package Trabalho_POO;
+
 public class UsuarioAssinante extends Usuario {
 
 	public UsuarioAssinante(String nome, String login, String senha, int idade) { 
 		super(nome,login,senha,idade);
+	}
+	
+	@Override
+	public double getPrecoIngresso(Sessao sessao){	
+		return 0.7 * super.getPrecoIngresso(sessao);
 	}
 	
 }
