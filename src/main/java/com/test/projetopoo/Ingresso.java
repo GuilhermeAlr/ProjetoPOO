4ca6eba7af7d34db808f32c4b4516663e90b0ffb
Isabela <167380547+isaaoki@users.noreply.github.com>
2024-10-26 11:57:16 -0300

    Create Ingresso.java

---
+++ src/main/java/com/test/projetopoo/Ingresso.java
@@ -0,0 +1,46 @@
+package main.java.com.test.projetopoo;
+
+public class Ingresso {
+    private Sessao sessao;
+    private int nroAssento;
+    private double precoIngresso;
+    
+    public Ingresso(Sessao sessao, int nroAssento, double precoIngresso) {
+        setSessao(sessao);
+        setNroAssento(nroAssento);;
+        setPrecoIngresso(precoIngresso);;
+    }
+    
+    public void setSessao(Sessao sessao) {
+        this.sessao = sessao;
+    }
+    
+    public Sessao getSessao() {
+        return this.sessao;
+    } 
+    
+    public void setNroAssento(int nroAssento) {
+        this.nroAssento = nroAssento;
+    }
+    
+    public int getNroAssento() {
+        return this.nroAssento;
+    }
+    
+    public void setPrecoIngresso(double precoIngresso) {
+        this.precoIngresso = precoIngresso;
+    }
+    
+    public double getPrecoIngresso() {
+        return this.precoIngresso;
+    }
+    
+    @Override
+    public String toString() { // podemos mudar para adequar layout da main
+        return "Filme: " + sessao.getFilmeSessao().getNomeFilme() + 
+               " - Data: " + sessao.getDiaSessao() + " " + sessao.getHorarioSessao() + 
+               " - Sala: " + sessao.getSalaSessao().getNroSala() + 
+               " - Poltrona: " + getNroAssento() + 
+               " - Preço: " + getPrecoIngresso();
+    }
+}
